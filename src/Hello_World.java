065c122b76d5f0f4820c29a6f86052f2fc340234
Aditya Verma <134534187+iamaditya442@users.noreply.github.com>
2024-02-23 11:51:21 +0530

    Update and rename Main.java to Hello_World.java

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Hello_World {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
